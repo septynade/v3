2a71fc5e134e4da01b5003dca02a3b5f434a847f
septynade <septynade@gmail.com>
2020-08-10 12:06:14 +0700

    fixed word spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,15 +13,14 @@ const Home = () => {
         <p className='text-body mt-4'>
           I'm an <strong>English literature</strong> graduate turned Web
           Developer.
-          <br />I was into Digital Marketing for a brief moment in my life,
+          <br /> I was a Digital Marketer for a brief moment in my life,
           <br />
           but then I realized it wasn't the kind of job I want to do for the
           rest of my life,
+          <br />I wanted something more nerdy and technical, and I found web
+          development to be quite fun,
           <br />
-          I wanted something more nerdy and technical.
-          <br />
-          And I found web development to be quite fun, that is until I met{' '}
-          <strong>JavaScript.</strong>
+          that is until I met <strong>JavaScript.</strong>
         </p>
         <a
           href='https://drive.google.com/file/d/1-F7fTQRqqa9NlVaJAE6upAG_rKysf9t7/view'
