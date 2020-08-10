3cf1d6ae3864f9df1776d1552eb5f51d46afc08f
septynade <septynade@gmail.com>
2020-08-10 09:50:09 +0700

    fixed some wordings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,10 +8,10 @@ const Contact = () => {
         <Col>
           <h1 className='text-center section-title'>What's Next?</h1>
           <p className='text-center text-body'>
-            I'm currently looking for an opportunity where I can put my
-            abilities to work and to keep growing and learning new things. Do
-            not hesitate to shoot me an email, I'll try my best to get back to
-            you as soon as posible!
+            I'm currently looking to join a team where I can put my abilities to
+            work and keep growing and learn new things. Do not hesitate to shoot
+            me an email, I'll try my best to get back to you as soon as
+            possible!
           </p>
           <a href='mailto:septynade@gmail.com' target='blank'>
             <button className='btn-white d-block mx-auto'>Email Me →</button>
