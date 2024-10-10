627b6023f954d873ad7ac1a6da0d3d1e85a8a971
Ade Septian <asmulyana@maybank.co.id>
2024-10-10 18:41:41 +0700

    small adjustment

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap'
-import pp from '../assets/pic.JPG'
+import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import pp from "../assets/pic.png";
 
 const About = () => {
   return (
-    <Container className='spacer' id='about'>
+    <Container className="spacer" id="about">
       <Row>
-        <Col style={{ marginRight: '2rem' }}>
-          <h1 className='section-title'>Hello!</h1>
-          <p className='mt-2 text-body'>
-            My name is Ade, I'm an English literature graduate turned
-            dev based in Indonesia.
+        <Col style={{ marginRight: "2rem" }}>
+          <h1 className="section-title">Hello!</h1>
+          <p className="mt-2 text-body">
+            My name is Ade, I'm an English literature graduate turned dev based
+            in Indonesia.
           </p>
-          <p className='text-body'>
-            I first dabbled with code on{' '}
+          <p className="text-body">
+            I first dabbled with code on{" "}
             <a
-              href='https://freecodecamp.org'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='link'
+              href="https://freecodecamp.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link"
             >
               <strong>FreeCodeCamp,</strong>
-            </a>{' '}
-            but only got really into it after a couple years of self-doubts, dozens of podcasts and success stories.
-            and in late 2019 I decided to quit my Digital Marketing job to focus
-            on learning to code and pursue a dev career. Since then,
-            I have been continuously learning the industries' best practices, new techs and stuff.
+            </a>{" "}
+            but only got really into it after a couple years of self-doubts,
+            dozens of podcasts and success stories. and in late 2019 I decided
+            to quit my Digital Marketing job to focus on learning to code and
+            pursue a dev career. Since then, I have been continuously learning
+            the industries' best practices, new techs and stuff.
           </p>
-          <p className='text-body'>
+          <p className="text-body">
             Here are a few technolgies that I'm experienced with:
           </p>
-          <ul className='tech-lists'>
-            <li className='tech-list'>Swift</li>
-            <li className='tech-list'>Objective-C</li>
-            <li className='tech-list'>UIKit</li>
-            <li className='tech-list'>Core Data</li>
-            <li className='tech-list'>JavaScript</li>
-            <li className='tech-list'>React</li>
+          <ul className="tech-lists">
+            <li className="tech-list">Swift</li>
+            <li className="tech-list">Objective-C</li>
+            <li className="tech-list">UIKit</li>
+            <li className="tech-list">Core Data</li>
+            <li className="tech-list">JavaScript</li>
+            <li className="tech-list">React</li>
           </ul>
         </Col>
-        <Col sm='12' md lg='6' xl='6' className='text-center pp'>
-          <Image width='275px' src={pp} className='pic'></Image>
+        <Col sm="12" md lg="6" xl="6" className="text-center pp">
+          <Image width="275px" src={pp} className="pic"></Image>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,82 +1,72 @@
-import React from 'react'
-import { Container, Row, Col, Card, CardGroup } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col, Card, CardGroup } from "react-bootstrap";
 
-import spurr from '../assets/spurr.png'
-import m2u from '../assets/m2u.jpg'
-
+import spurr from "../assets/spurr.png";
+import m2u from "../assets/m2u.jpg";
 
 const Project = () => {
   return (
-    <Container fluid style={{ background: '#121212', padding: '2em 0' }}>
-      <Container className='mb-5' id='project'>
-        <Row className='justify-content-end'>
+    <Container fluid style={{ background: "#121212", padding: "2em 0" }}>
+      <Container className="mb-5" id="project">
+        <Row className="justify-content-end">
           <Col>
-            <h1 className='text-center mb-2 section-title'>Projects I've worked on</h1>
-            <hr className='mb-5' />
+            <h1 className="mb-2 section-title">Projects I've worked on</h1>
+            <hr className="mb-5" />
             <Row>
               <Col>
-                <CardGroup className='mb-1'>
-                  <Card className='px-4 pt-3'>
+                <CardGroup className="mb-1">
+                  <Card className="px-4 pt-3">
                     <a
-                      href='https://www.maybank.co.id/'
-                      target='_blank'
-                      rel='noopener noreferrer'
+                      href="https://www.maybank.co.id/"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <Card.Img
-                        variant='top'
-                        src={m2u}
-                        className='pro-img'
-                      />
+                      <Card.Img variant="top" src={m2u} className="pro-img" />
                     </a>
                     <Card.Body>
                       <Card.Title>
                         M2U Indonesia
                         <a
-                          href='https://www.maybank.co.id/'
-                          target='_blank'
-                          rel='noopener noreferrer'
+                          href="https://www.maybank.co.id/"
+                          target="_blank"
+                          rel="noopener noreferrer"
                         >
-                          <i className='fas fa-external-link-alt pro-link ml-3'></i>
+                          <i className="fas fa-external-link-alt pro-link ml-3"></i>
                         </a>
                       </Card.Title>
+                      <Card.Text>A banking app.</Card.Text>
                       <Card.Text>
-                      A banking app.
-                      </Card.Text>
-                      <Card.Text>
-                        <small className='text-muted'>
+                        <small className="text-muted">
                           Built with: Swift, Objective-C, MVVM, UIKit
                         </small>
                       </Card.Text>
                     </Card.Body>
                   </Card>
-                  <Card className='px-4 pt-3'>
+                  <Card className="px-4 pt-3">
                     <a
-                      href='https://gospurr.com'
-                      target='_blank'
-                      rel='noopener noreferrer'
+                      href="https://gospurr.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <Card.Img
-                        variant='top'
-                        src={spurr}
-                        className='pro-img'
-                      />
+                      <Card.Img variant="top" src={spurr} className="pro-img" />
                     </a>
                     <Card.Body>
                       <Card.Title>
                         Spurr
                         <a
-                          href='https://gospurr.com'
-                          target='_blank'
-                          rel='noopener noreferrer'
+                          href="https://gospurr.com"
+                          target="_blank"
+                          rel="noopener noreferrer"
                         >
-                          <i className='fas fa-external-link-alt pro-link ml-3'></i>
+                          <i className="fas fa-external-link-alt pro-link ml-3"></i>
                         </a>
                       </Card.Title>
                       <Card.Text>
-                      A simple yet powerful customer loyalty solution suitable for all brick-and-mortar retail and services.
+                        A simple yet powerful customer loyalty solution suitable
+                        for all brick-and-mortar retail and services.
                       </Card.Text>
                       <Card.Text>
-                        <small className='text-muted'>
+                        <small className="text-muted">
                           Built with: Swift, MVVM, UIKit
                         </small>
                       </Card.Text>
@@ -89,7 +79,7 @@ const Project = () => {
         </Row>
       </Container>
     </Container>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
